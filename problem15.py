028ed41cec9d9adff4659ca10e3a71b5da1c286f
Jahanvi26807khandve <jahanvi26807khandve@gmail.com>
2026-08-04 22:39:50 +0530

    ..

---
+++ problem15.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your number: "))
+
+print("the square of your number is", a**2)
+
+# a^2 then in python it will give you the value of a XOR 2 not square of a. So, to get the square of a we use a**2  
---
+++ problem17.py
@@ -0,0 +1,7 @@
+# STRINGS 
+name = "Jahanvi"
+
+nameshort = name[0:3] # short string
+character = name[1]
+print(nameshort)
+print(character)
